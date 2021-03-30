d544daa599a280bb16209d025adaae8e71cb3c8e
Egor <znogoud3@gmail.com>
2021-03-30 20:36:32 +0300

    self editing start

--- src/index.js
+++ src/index.js
@@ -3,29 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let posts = [
-  { id: 1, post: "Not a spoon, not a fork, but something in between.", likesCount: "125" },
-  { id: 2, post: "I LOVE BEING IN CONTINENT!", likesCount: "225" },
-  { id: 3, post: "I HAVE WHAT I'M HAVING!", likesCount: "135" },
-]
-
-let dialogs = [
-  { id: 1, name: "Bogdan" },
-  { id: 2, name: "Vladimir" },
-  { id: 3, name: "Alexey" },
-  { id: 4, name: "Alexandr" }
-]
-
-let messages = [
-  { id: 1, message: "Bodyaaaaa!" },
-  { id: 2, message: "I'm wide" },
-  { id: 3, message: "I'm in prison" },
-]
-
+import state from './redux/state';
+ 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+
+let state = {
+    profilePage: {
+        posts: [
+            { id: 1, post: "Not a spoon, not a fork, but something in between.", likesCount: "125" },
+            { id: 2, post: "I LOVE BEING IN CONTINENT!", likesCount: "225" },
+            { id: 3, post: "I HAVE WHAT I'M HAVING!", likesCount: "135" },
+        ],
+    },
+    dialogsPage: {
+        messages: [
+            { id: 1, message: "Bodyaaaaa!" },
+            { id: 2, message: "I'm wide" },
+            { id: 3, message: "I'm in prison" },
+        ],
+        dialogs: [
+            { id: 1, name: "Bogdan" },
+            { id: 2, name: "Vladimir" },
+            { id: 3, name: "Alexey" },
+            { id: 4, name: "Alexandr" }
+        ],
+    }
+}
+
+export default state;
