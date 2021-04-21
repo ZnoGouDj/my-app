c177f76a04280a5aa96581f97910fa80102686a1
Egor <znogoud3@gmail.com>
2021-04-21 23:22:54 +0300

    users added, api created, users class created

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
+import UsersContainer from './components/Users/UsersContainer';
 
 const App = () => {
   return (
@@ -14,11 +12,14 @@ const App = () => {
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
-        <Route path="/dialogs" render={() => <DialogsContainer />} />
-        <Route path="/profile" render={() => <Profile />} />
-        <Route path="/news" component={News} />
-        <Route path="/music" component={Music} />
-        <Route path="/settings" component={Settings} />
+        <Route path="/dialogs"
+          render={() => <DialogsContainer />} />
+
+        <Route path="/profile"
+          render={() => <Profile />} />
+
+        <Route path="/users"
+          render={() => <UsersContainer /> } />
       </div>
     </div>
   );
