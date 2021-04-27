718637c28b119220117b7d500947d21a423a0df1
Egor <znogoud3@gmail.com>
2021-04-27 21:12:17 +0300

    pagination added, UsersContainer component added, ajax removed to the container component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,48 +1,49 @@
 import React from 'react';
-import styles from './users.module.css';
-import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
+import styles from './users.module.css';
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    constructor(props) {
-        super(props);
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            });
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    render() {
-        return <div>
-            {
-                this.props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="userPhoto" />
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-                                : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>
-                            }
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            <div>{"u.location.country"}</div>
-                            <div>{"u.location.city"}</div>
-                        </span>
-                    </span>
-                </div>)
-            }
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage}
+                    onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
+            })}
         </div>
-    }
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="userPhoto" />
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>
+                        }
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </span>
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
