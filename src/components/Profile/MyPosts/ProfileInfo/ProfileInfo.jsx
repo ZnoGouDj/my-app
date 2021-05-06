2ca554cc8326958ebb0c62ed9298fe0502ab2073
Egor <znogoud3@gmail.com>
2021-05-06 23:54:14 +0300

    withRouter added

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -13,7 +13,7 @@ const ProfileInfo = (props) => {
                 <img className={s.avatar} alt="background" src='https://pbs.twimg.com/profile_images/783445386375507969/nTv88w7E_400x400.jpg'></img>
             </div>
             <div className={s.descriptionBlock}>
-                {/* <img src={props.profile.photos.large} alt="img"/> */}
+                <img src={props.profile.photos.large} alt="img"/>
                 Jordan Peele
             </div>
         </div>
