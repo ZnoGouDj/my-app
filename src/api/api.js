787adb8917e4293e37df0f9841bc1ce81c0db22f
Egor <znogoud3@gmail.com>
2021-06-01 12:08:32 +0300

    redux-think details added pt.2

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import * as axios from "axios";
 const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers:     {
+    headers: {
         "API-KEY": "365d74b6-fdd2-429e-b94f-c75d8c420eb2"
     }
 });
@@ -16,5 +16,11 @@ export const usersAPI = {
             .then(response => {
                 return response.data;
             });
+    },
+    follow(userId) {
+        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     }
 }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import * as axios from "axios";
+import { usersAPI } from '../../api/api';
 
 let Users = (props) => {
 
@@ -18,56 +19,27 @@ let Users = (props) => {
         <div>
             {pages.map(p => {
                 return <span className={props.currentPage === p && styles.selectedPage}
-                             onClick={(e) => {
-                                 props.onPageChanged(p);
-                             }}>{p}</span>
+                    onClick={(e) => {
+                        props.onPageChanged(p);
+                    }}>{p}</span>
             })}
         </div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                       <NavLink to={'/profile/' + u.id}>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                             className={styles.userPhoto} alt="userPhoto"/>
-                       </NavLink>
+                        <NavLink to={'/profile/' + u.id}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                                className={styles.userPhoto} alt="userPhoto" />
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed
                             ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.toggleFollowingProgress(true, u.id);
-                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
-                                    withCredentials: true,
-                                    headers: {
-                                        "API-KEY": "365d74b6-fdd2-429e-b94f-c75d8c420eb2"
-                                    }
-                                })
-                                    .then(response => {
-                                        if (response.data.resultCode === 0) {
-                                            props.unfollow(u.id);
-                                        }
-                                        props.toggleFollowingProgress(false, u.id);
-                                    });
-
-
-
+                                props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.toggleFollowingProgress(true, u.id);
-                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
-                                    withCredentials: true,
-                                    headers: {
-                                        "API-KEY": "365d74b6-fdd2-429e-b94f-c75d8c420eb2"
-                                    }
-                                })
-                                    .then(response => {
-                                        if (response.data.resultCode === 0) {
-                                            props.follow(u.id);
-                                        }
-                                        props.toggleFollowingProgress(false, u.id);
-                                    });
-
-
+                                props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
