1ac933e9e51d2fb2f86bf2864c51afd9e74f70e9
Egor <znogoud3@gmail.com>
2021-06-04 21:46:52 +0300

    High ordered components implemented

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,7 @@ import {
 } from '../../redux/users-reducer';
 import Users from './Users';
 import Preloader from "../common/Preloader/Preloader";
+import { withAuthRedirect } from '../../hoc/WithAuthRedirect';
 
 
 
@@ -48,5 +49,5 @@ let mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps,
-    { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers })(UsersContainer);
+export default withAuthRedirect(connect(mapStateToProps,
+    { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers })(UsersContainer));
